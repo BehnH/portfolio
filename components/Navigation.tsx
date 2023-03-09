b8a1acd429466b418f4200d3357b53de35927c1d
Behn <7383025+BehnH@users.noreply.github.com>
2023-03-09 09:00:46 +0000

    components: Add blog to navbar

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -73,6 +73,31 @@ export default function Navbar() {
                             )}
                         </a>
                     </Link>
+                    <Link href="/blog" legacyBehavior>
+                        <a
+                            className={`text-base  ${router.asPath === "/blog"
+                                    ? "text-gray-800 font-bold"
+                                    : "text-gray-600 font-normal "
+                                }`}
+                        >
+                            Blog{" "}
+                            {router.asPath === "/blog" && (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                    fill="currentColor"
+                                    className="bi bi-arrow-down inline-block h-3 w-3"
+                                    viewBox="0 0 16 16"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                                    />
+                                </svg>
+                            )}
+                        </a>
+                    </Link>
                     <Link href="/listening" legacyBehavior>
                         <a
                             className={`text-base  ${router.asPath === "/listening"
