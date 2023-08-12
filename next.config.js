1b16094c0523b50a15f38d183ef560a9fdb48daa
Behn <7383025+BehnH@users.noreply.github.com>
2023-08-12 09:07:21 +0100

    app: Don't use standalone output mode

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  output: "standalone"
 }
 
 module.exports = nextConfig
