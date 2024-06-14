cbdbc13fc9c513e7c70ec412b417b21398998cc3
Behn <7383025+BehnH@users.noreply.github.com>
2024-06-14 21:39:08 +0100

    Update blog layout

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import { ContainerBlock } from "../../components/Base";
 import MDXComponents from "../../components/MdxComps";
 import { getPostBySlug, getPostDir } from "../../utils/mdx";
+import Link from "next/link";
 
 const Blogpost = ({ mdxSource, frontMatter }: any) => {
     const { title, date, readingTime, description, slug, bannerfile } = frontMatter;
@@ -22,19 +23,33 @@ const Blogpost = ({ mdxSource, frontMatter }: any) => {
                 publishedDate={format(parseISO(date), "yyyy-MM-ddTHH:mm:sszzz")}
             >
                 <link rel="stylesheet" href={"/styles/github-dark.min.css"} />
-                <div className="max-w-4xl mx-auto pb-5">
-                    <h1 className="text-3xl font-semibold underline decoration-[#bbf7d0] decoration-wavy decoration-2">{title}</h1>
+                <div className="max-w-4xl mx-auto pb-5 text-center">
                     <div className="flex flex-row justify-between">
-                        <div className="flex flex-row gap-2 mt-2">
+                        <Link
+                          className={"flex flex-row gap-2 px-10 text-gray-500"}
+                          href={"/blog"}
+                          passHref={true}>
+                            <i className="ri-arrow-left-line ri-md"></i>
+                            <p>Go back to the list</p>
+                        </Link>
+                        <div className="flex flex-row gap-2 mb-2">
                             <i className="ri-calendar-2-line ri-md"></i>
                             <p className="text-gray-500 text-md">
                                 {format(parseISO(date), "MMMM dd, yyyy")}
                             </p>
                         </div>
+                        <div className={"flex flew-row gap-2 mb-2"}>
+                            <i className="ri-time-line"></i>
+                            <p className="text-gray-500 text-md">
+                                {readingTime.text}
+                            </p>
+                        </div>
                     </div>
+                    <h1
+                      className="text-3xl font-semibold underline decoration-[#bbf7d0] decoration-wavy decoration-2">{title}</h1>
                 </div>
-                <div className="max-w-2xl mx-auto prose prose-violet pb-10">
-                    <MDXRemote {...mdxSource} components={{ ...MDXComponents }} />
+                <div className="max-w-5xl mx-auto prose prose-violet pb-10">
+                    <MDXRemote {...mdxSource} components={{...MDXComponents}}/>
                 </div>
             </ContainerBlock>
         </>
